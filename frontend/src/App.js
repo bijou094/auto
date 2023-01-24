6bb25a90b0849526acd370184e782a753622e34c
bouaka houda <bouaka.houda@outlook.fr>
2023-01-24 10:21:17 +0100

    ajouter l'app react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+//import RouteCompenent from './routes/RouteCompenent'<RouteCompenent />
+
+function App() {
+  return (
+    <div className="App">
+      
+
+    </div>
+  );
+}
+
+export default App;
