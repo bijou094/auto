2137d6ca934be96e300d512026dfcc5d9c843c4c
bouaka houda <bouaka.houda@outlook.fr>
2023-01-26 21:40:33 +0100

    ajouter les dépendances et criée  un server

--- frontend/src/compenents/Header.js
+++ frontend/src/compenents/Header.js
@@ -1,11 +1,16 @@
 import Navigator from "./Navigator";
+import logo from '../images/logo.jpg'
+import '../styles/header.css'
 const Header =()=>{
 
     return(
         <>
-            <div>
-                <div>
-                <h2>logo</h2>
+            <div className='header' >
+                <div className='header_navigation' >
+                
+                    <div className='header_image'>
+                        <img src={logo} className='logo' alt='header_logo' />
+                    </div>
                 </div>
                 <Navigator/>
             </div>
